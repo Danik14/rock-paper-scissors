9e1709331158e848dc1300e31a0c8b087c2d1ba1
Danik <chapagana@gmail.com>
2021-12-12 23:50:48 +0600

    added start button

---
+++ index.js
@@ -0,0 +1,58 @@
+function computerPlay() {
+  const hands = ["rock", "paper", "scissors"];
+  const hand = hands[Math.floor(Math.random() * hands.length)];
+  return hand;
+}
+
+function playerSelect() {
+  let hand = prompt("Choose your fighter (rock, paper, scissors)");
+  hand = hand.toLowerCase();
+  return hand;
+}
+
+function versus() {
+  const computer = computerPlay();
+  const player = playerSelect();
+
+  if (computer == player) {
+    return 0;
+  } else if (player == "scissors" && computer == "rock") {
+    return -1;
+  } else if (player == "scissors" && computer == "paper") {
+    return 1;
+  } else if (player == "rock" && computer == "scissors") {
+    return 1;
+  } else if (player == "rock" && computer == "paper") {
+    return -1;
+  } else if (player == "paper" && computer == "scissors") {
+    return -1;
+  } else if (player == "paper" && computer == "rock") {
+    return 1;
+  }
+}
+
+function game() {
+  let computer = 0;
+  let player = 0;
+
+  while (player < 3 && computer < 3) {
+    let game = versus();
+    if (game == 1) {
+      player++;
+    } else if (game == -1) {
+      computer++;
+    }
+    alert("Player: " + player + " Computer: " + computer);
+  }
+
+  alert(
+    player > computer
+      ? "You won!\n" + " Player: " + player + " Computer: " + computer
+      : "You lost :( \n" + " Player: " + player + " Computer: " + computer
+  );
+}
+
+const button = document.querySelector(".raise");
+button.addEventListener("click", (e) => {
+  e.target.remove();
+});
